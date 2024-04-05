fb0bd5f13e19100cec578fb1e56507b608d9385c
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-05 23:42:41 +0330

    feat : store auth mode in params

--- src/app/auth/client.tsx
+++ src/app/auth/client.tsx
@@ -2,8 +2,28 @@
 
 import SignupForm from "@/components/signupForm/SignupForm";
 import styles from "./authPage.module.css"
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const Client = () => {
+
+    const signupSwitch = "قبلا ثبت نام کردی ؟ ";
+    const loginSwitch = "هنوز ثبت نام نکردی؟";
+
+    const {replace} = useRouter();
+    
+    const searchParams = useSearchParams();
+    
+    const path = usePathname();
+    
+    const params = new URLSearchParams(searchParams);
+
+    const authMode = params.get("authMode") || "signup";
+
+    const handleSwitch = () => {
+        params.set("authMode",authMode === "signup" ? "login" : "signup");
+        replace(`${path}?${params}`);
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
@@ -11,6 +31,11 @@ const Client = () => {
                     به ایومیل خوش اومدی
                 </p>
                 <SignupForm />
+                <p className={styles.switch} onClick={handleSwitch}>
+                    {
+                        authMode === "signup" ? signupSwitch : loginSwitch
+                    }
+                </p>
             </div>   
         </div>
     )
