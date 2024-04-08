d88213d0e8797d87e1b7a8ad36ee1004a16c1e4b
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-08 20:00:38 +0330

    feat : use types

--- src/store/useMessages.ts
+++ src/store/useMessages.ts
@@ -1,11 +1,12 @@
+import { MessageType } from '@/types'
 import { create } from 'zustand'
 
 interface useMessagesState {
-    messages: any[]
-    setMessages: (messages: any) => void
+    messages: MessageType[]
+    setMessages: (messages: MessageType[]) => void
 }
 
 export const useBearStore = create<useMessagesState>()((set) => ({
     messages: [],
-    setMessages: (messages) => set({messages}),
+    setMessages: (messages:MessageType[]) => set({messages}),
 }))
