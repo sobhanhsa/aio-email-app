772375fe8eb8e83d0a287614bcbc0de935cf4c29
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-08 19:59:21 +0330

    feat : declare message type

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,15 @@ export type UserType = {
     hash:string,
     messages:any[] | []
 }
+
+export type MessageType = {
+    _id:string,
+    createdAt:Date,
+    updatedAt:Date,
+    receivers:string | UserType[],
+    sender:string | UserType,
+    subject:string,
+    body:string,
+    isReplied:boolean,
+    repliedTo:string | MessageType
+};
