9ed23ca3b027f828c613c5ec955ba0a6aef6ebe4
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-10 09:24:51 +0330

    feat : make messages dynamic

--- src/components/messages/Messages.tsx
+++ src/components/messages/Messages.tsx
@@ -1,21 +1,30 @@
+"use client"
+
 import Message from "@/components/message/Message";
 import styles from "./messages.module.css"
+import { useMessagesStore } from "@/store/useMessages";
+import { MessageType } from "@/types";
+import { useListenMessages } from "@/hooks/useListenMessages";
+import { useGetMessages } from "@/hooks/useGetMessages";
 
 const  Messages = () => {
+    useListenMessages();
+    useGetMessages();
+    const {messages}:{messages:MessageType[]} = useMessagesStore();
+    console.log(messages)
     return (
         <div className={styles.container}>
-            <Message sender="sobhan"
-                subject="سلام یرگه"
-                date="2024/02/11"
-            />
-            <Message sender="sobhan"
-                subject="سلام یرگه"
-                date="2024/02/11"
-            />
-            <Message sender="sobhan"
-                subject="سلام یرگه"
-                date="2024/02/11"
-            />   
+            {
+                messages.map((msg:MessageType) => (
+                    <Message
+                        id={msg._id}
+                        key={msg._id}
+                        sender={msg.sender.username}
+                        subject={msg.subject}
+                        date={"12324"||msg.createdAt}
+                    />
+                ))
+            }
         </div>
     )
 };
