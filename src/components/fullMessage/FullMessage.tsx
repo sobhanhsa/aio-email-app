1c0eabd2b9c3a358a12c8f6860f81a28d56f854e
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-10 12:51:45 +0330

    feat : init full message component

---
+++ src/components/fullMessage/FullMessage.tsx
@@ -0,0 +1,11 @@
+import styles from "./fullMessage.module.css"
+
+const  FullMessage = () => {
+    return (
+        <div className={styles.container}>
+            FullMessage   
+        </div>
+    )
+};
+
+export default FullMessage;
