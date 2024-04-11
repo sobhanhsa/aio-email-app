5450831bc0b9681aa6d8bae789a8018af8ca2fdc
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-11 11:53:29 +0330

    feat : make single page dynamic

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -6,55 +6,73 @@ import { MessageType, UserType } from "@/types";
 import { useAuthContext } from "@/context/authContext";
 import FullMessage from "@/components/fullMessage/FullMessage";
 import { useEffect, useRef } from "react";
+import { useGetRelatedMessages } from "@/hooks/useGetRelatedMessages";
 
 const  SelectedMessagesSinglePage = ({params}:{
     params:{
         id:string
     }
 }) => {
-    const {messages} = useMessagesStore();
-    const selectedMessage = messages.find(msg => msg._id === params.id);
-    if (!selectedMessage) {
-        return (<div>
-            چنین پیامی وجود ندارد
-        </div>)
-    };
-    const {authUser}:{authUser:UserType} = useAuthContext();
-    const secondaryPerson : UserType = selectedMessage?.sender !== authUser 
-    ? selectedMessage.sender
-    : selectedMessage.receivers[0];
-    const relatedMessages = messages.filter((msg:MessageType) => {
-        return ( msg.sender._id === secondaryPerson._id 
-            || msg.receivers[0]._id === secondaryPerson._id) 
-    });
-
+    // const {messages} = useMessagesStore();
+    // const selectedMessage = messages.find(msg => msg._id === params.id);
+    // if (!selectedMessage) {
+    //     return (<div>
+    //         چنین پیامی وجود ندارد
+    //     </div>)
+    // };
+    // const {authUser}:{authUser:UserType} = useAuthContext();
+    // const secondaryPerson : UserType = selectedMessage?.sender !== authUser 
+    // ? selectedMessage.sender
+    // : selectedMessage.receivers[0];
+    // const relatedMessages = messages.filter((msg:MessageType) => {
+    //         return ( msg.sender._id === secondaryPerson._id 
+    //             || msg.receivers[0]._id === secondaryPerson._id) 
+    //     });
+        
+        
     const selectedMassageRef = useRef(null);
 
-    // console.log("singlePage :",secondaryPerson);
-    // console.log("singlePage-messages :",messages);
+    const {
+        relatedMessages,
+        loading,
+        secondaryPerson
+    } = useGetRelatedMessages(params.id);
+
+
     
     useEffect(()=> {
         (selectedMassageRef.current as any)
-            ?.scrollIntoView({ behavior: "instant" , block:"center"});
-    },[]);
-
+        ?.scrollIntoView({ behavior: "instant" , block:"center"});
+    },[relatedMessages]);
+    
+    if (!loading && !relatedMessages) {
+        return <div>
+            چنین پیامی یافت نشد
+        </div>
+    }
     return (
         <div className={styles.container}>
             {
-                relatedMessages.map(msg => {
+                relatedMessages?.map((msg:MessageType) => {
+                    console.log("map : ",msg._id.toString() === params.id);
                     // secondary person role!
-                    const sPRole = msg.sender === secondaryPerson 
-                        ? "sender"
-                        : "receiver"
+                    const sPRole = msg.sender
+                        ._id.toString() === secondaryPerson?._id.toString() 
+                            ? "sender"
+                            : "receiver"
+                    
                     return (
                         <FullMessage
                             key={msg._id}
                             message={msg}
                             secondaryPerson={{
-                                user:secondaryPerson,
+                                user:secondaryPerson as UserType,
                                 role:sPRole
                             }}
-                            selectedRef={msg._id === params.id ? selectedMassageRef : null}
+                            selectedRef={msg._id.toString() === params.id 
+                                ? selectedMassageRef 
+                                : null
+                            }
                         />
                     )
                 })
