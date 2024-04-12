28b6b0d4f359e06bbc65ea86cfb482c763057279
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-12 15:02:22 +0330

    feat : remove logout btn

--- src/components/sideBar/sideBar.tsx
+++ src/components/sideBar/sideBar.tsx
@@ -1,15 +1,11 @@
 import { MdAdd } from "react-icons/md";
 import styles from "./sideBar.module.css"
 import { IoLogOutOutline } from "react-icons/io5";
-import LogoutButton from "../logoutButton/LogoutButton";
 import Link from "next/link";
 
 const  SideBar = () => {
     return (
         <div className={styles.container}>
-            <div>
-                <LogoutButton />
-            </div>
             <Link href={"/compose"}>
                 <div className={styles.compose}>
                     نوشتن
